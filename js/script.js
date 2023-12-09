0a7805013e60120a97a5d71ec1fb8a89e9e9f366
artur87a <artur.kodehode@gmail.com>
2023-12-09 19:05:39 +0100

    v2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {;
 
-const nav = document.querySelector(".navbar");
+const nav = document.querySelector(".navbar")
+const allNavItems = document.querySelectorAll(".nav-link")
+const navList = document.querySelector(".navbar-collapse")
+
 
 function addShadow() {
   if (window.scrollY >= 300) {
@@ -8,9 +11,13 @@ function addShadow() {
   } else {
     nav.classList.remove("shadow-bg");
   }
-
   
 }
+
+allNavItems.forEach(item => item.addEventListener("click", () =>
+  navList.classList.remove("show")
+
+))
     window.addEventListener("scroll", addShadow)
 })
 
